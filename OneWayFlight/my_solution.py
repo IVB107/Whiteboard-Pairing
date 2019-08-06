c23055ba284e4777b1dac90ba9c6577df33b610a
IVB107 <ianvb107@gmail.com>
2019-08-06 11:26:01 -0500

    Added comments for clarity

--- OneWayFlight/my_solution.py
+++ OneWayFlight/my_solution.py
@@ -1,17 +1,20 @@
 def reconstructTrip(tickets):
+  # This solution is O(2n) --> O(n)
   flights = {}
   route = []
   departure = None
+  
+  # Add each source: destination pair to dictionary
   for ticket in tickets:
     if ticket[0] is None:
       departure = ticket[1]
     flights[ticket[0]] = ticket[1]
   
+  # Add each destination to list until none exists
   while flights[departure] is not None:
     route.append(flights[departure])
     departure = flights[departure]
 
-  # print(f'Flights: {flights}')
   print(f'Route: {route}')
 
 tix = [
